e36faba65d0b06b9516a2e7dfe71df5609b48372
Lxc <63281620+lixiaochjajo@users.noreply.github.com>
2021-12-21 16:03:26 +0800

    fix: 扫光效果和渐变色

--- src/components/scene.vue
+++ src/components/scene.vue
@@ -15,7 +15,6 @@ export default {
   mounted() {
     this.init();
     this.createControls();
-
     this.addGLTF();
     this.render();
   },
@@ -52,65 +51,25 @@ export default {
       controls = new OrbitControls(camera, renderer.domElement);
     },
     render() {
+      this.cityanimate();
       renderer.render(scene, camera);
       requestAnimationFrame(this.render); // 请求再次执行渲染函数render
     },
-     addGLTF() {
+    addGLTF() {
       const loader = new GLTFLoader();
       loader.load("shanghai.gltf", (gltf) => {
         gltf.scene.traverse((child) => {
           // 设置线框材质
-
           if (child.isMesh) {
             //这个判断模型是楼房还是其他  加载不同的材质
             if (["CITY_UNTRIANGULATED"].includes(child.name)) {
               // 拿到模型线框的Geometry
-              const edges = new THREE.EdgesGeometry(child.geometry, 1);
-              //设置模型的材质
-              const lineMaterial = new THREE.LineBasicMaterial({
-                // 线的颜色
-                color: "rgba(38,133,254)",
-              });
-              //把数据组合起来
-              const lineS = new THREE.LineSegments(edges, lineMaterial);
-              //设置数据的位置
-              lineS.position.set(
-                child.position.x,
-                child.position.y,
-                child.position.z
-              );
-              //添加到场景
-              scene.add(lineS);
-              lineS.rotateX(-Math.PI / 2);
-              // 模型面材质
-              const material = new THREE.MeshPhysicalMaterial({
-                //颜色为
-                color: "rgb(50,170,255)",
-                //金属度
-                metalness: 0.5,
-                //粗糙度
-                roughness: 0.1,
-                //透明度
-                transmission: 0.9,
-                //模型是否透明
-                transparent: true,
-              });
-              //生成模型对象
-              const mesh = new THREE.Mesh(child.geometry, material);
-              //添加到场景
-              scene.add(mesh);
-              mesh.position.set(
-                child.position.x,
-                child.position.y,
-                child.position.z
-              );
-
-              mesh.rotateX(-Math.PI / 2);
+              this.setCityLineMaterial(child);
+              this.setCityMaterial(child);
             } else if (["ROADS"].includes(child.name)) {
               //道路
               const material = new THREE.MeshBasicMaterial({
                 color: "rgb(41,46,76)",
-           
               });
               const mesh = new THREE.Mesh(child.geometry, material);
               mesh.rotateX(-Math.PI / 2);
@@ -133,14 +92,113 @@ export default {
                 child.position.y,
                 child.position.z
               );
-          
             }
           }
           // 设置线框材质
         });
       });
     },
-  
+    setCityMaterial(object) {
+      const shader = new THREE.ShaderMaterial({
+        uniforms: {
+          height: this.height,
+          uFlowColor: {
+            value: new THREE.Color("#5588aa"),
+          },
+          uCityColor: {
+            value: new THREE.Color("#1B3045"),
+          },
+        },
+        vertexShader: `
+                varying vec3 vPosition;
+                void main()
+                {
+                  vPosition = position;
+                  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+                }`,
+        fragmentShader: `
+float distanceTo(vec2 src,vec2 dst)
+{
+    float dx=src.x-dst.x;
+    float dy=src.y-dst.y;
+    float dv=dx*dx+dy*dy;
+    return sqrt(dv);
+}
+varying vec3 vPosition;
+uniform float height;
+uniform float uStartTime;
+uniform vec3 uSize;
+uniform vec3 uFlowColor;
+uniform vec3 uCityColor;
+void main()
+{
+    //模型的基础颜色
+    vec3 distColor=uCityColor;
+    // 流动范围当前点z的高度加上流动线的高度
+    float topY=vPosition.z+10.;
+    if(height>vPosition.z&&height<topY){
+        // 颜色渐变
+            float dIndex = sin((height - vPosition.z) / 10.0 * 3.14);
+            distColor = mix(uFlowColor, distColor, 1.0-dIndex);
+
+    }
+    //定位当前点位位置
+    vec2 position2D=vec2(vPosition.x,vPosition.y);
+    //求点到原点的距离
+    float Len=distanceTo(position2D,vec2(0,0));
+      if(Len>height*30.0&&Len<(height*30.0+130.0)){
+        // 颜色渐变
+        float dIndex = sin((Len - height*30.0) / 130.0 * 3.14);
+        distColor= mix(uFlowColor, distColor, 1.0-dIndex);
+    }
+    gl_FragColor=vec4(distColor,1.0);
+}`,
+                    transparent: true,
+      });
+
+      const city = new THREE.Mesh(object.geometry, shader);
+      city.position.set(
+        object.position.x,
+        object.position.y,
+        object.position.z
+      );
+      scene.add(city);
+      city.rotateX(-Math.PI / 2);
+    },
+    setCityLineMaterial(object) {
+      const edges = new THREE.EdgesGeometry(object.geometry, 1);
+      //设置模型的材质
+      const lineMaterial = new THREE.LineBasicMaterial({
+        // 线的颜色
+        color: "rgba(38,133,254)",
+      });
+      //把数据组合起来
+      const lineS = new THREE.LineSegments(edges, lineMaterial);
+      //设置数据的位置
+      lineS.position.set(
+        object.position.x,
+        object.position.y,
+        object.position.z
+      );
+      //添加到场景
+      scene.add(lineS);
+      lineS.rotateX(-Math.PI / 2);
+    },
+    cityanimate() {
+      this.height.value += 0.2;
+      if(this.height.value>100)
+      {
+        this.height.value=0.0
+      }
+    
+    },
+  },
+  data() {
+    return {
+      height: {
+        value: 0,
+      },
+    };
   },
 };
 </script>
